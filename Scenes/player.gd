db9816626c3c4f8188aa87b758a3389292159631
prayagajith <prayagajith2007@gmail.com>
2025-09-05 01:19:05 +0530

    playermovement

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,11 +6,11 @@ const FALL_VELOCITY = 300
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	animated_sprite_2d.animation
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		animated_sprite_2d.play("jump")
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -24,8 +24,11 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.flip_h = false
 	elif direction < 0:
 		animated_sprite_2d.flip_h = true
+	elif direction == 0 and is_on_floor():
+		animated_sprite_2d.play("idle")
 	if direction:
 		velocity.x = direction * SPEED
+		animated_sprite_2d.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
