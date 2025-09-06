b86db8a908a5a7d9692fc6bd97e6eb1b5d9d915c
prayagajith <prayagajith2007@gmail.com>
2025-09-07 02:42:56 +0530

    enemy collision fix

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,6 +1,7 @@
 extends Area2D
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var ray_cast_2d_2: RayCast2D = $RayCast2D2
+@onready var ray_cast_2d_3: RayCast2D = $RayCast2D3
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: CharacterBody2D = $"../Player"
 
@@ -10,12 +11,15 @@ var s=100
 var d=1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	ray_cast_2d.add_exception(player)
+	ray_cast_2d_2.add_exception(player)
 	animated_sprite_2d.play("enemyani")
 	if ray_cast_2d.is_colliding():
 		d=-1
