d45b2de2c88c16ed965d1b0b3d36f54739b63b05
prayagajith <prayagajith2007@gmail.com>
2025-09-16 05:06:27 +0530

    improvement on player attack

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 #@onready var kill_zone: Area2D = $"Kill zone"
 @onready var enemy: Area2D = $"../enemy"
-@onready var timer: Timer = $Timer
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var attacol: CollisionShape2D = $Area2D/attacol
 @onready var attack: Sprite2D = $Area2D/attack
